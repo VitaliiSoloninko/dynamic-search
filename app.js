6f4be21eedde1a836b8bcf589710581c46b1ada2
22051988Di <vitalii.soloninko@gmail.com>
2024-06-17 15:39:56 +0200

    v0.2	Responce out server and parsing to json

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 async function start() {
 	try {
+		const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+		const data = await resp.json()
+		console.log(resp)
+		console.log(data)
 	} catch (err) {}
 }
 
