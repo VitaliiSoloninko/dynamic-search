8ca69e8fbc00a281da639497302cb249b72a4c1a
22051988Di <vitalii.soloninko@gmail.com>
2024-06-17 15:51:53 +0200

    v0.3	Show users in HTML

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+const list = document.querySelector('#list')
+
 async function start() {
 	try {
 		const resp = await fetch('https://jsonplaceholder.typicode.com/users')
 		const data = await resp.json()
 		console.log(resp)
 		console.log(data)
+		renderUsers(data)
 	} catch (err) {}
+}
+
+function renderUsers(users = []) {
+	const html = users.map(toHTML).join('')
+	list.innerHTML = html
+}
+
+function toHTML(user) {
+	return `
+	<li class="list-group-item">${user.name}</li>
+	`
 }
 
 start()
