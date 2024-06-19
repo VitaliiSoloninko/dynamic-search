fa6b237b0f6190489257156a9c2886592410ad1f
22051988Di <vitalii.soloninko@gmail.com>
2024-06-19 16:46:42 +0200

    v0.6	Filter part 2, remember users and filter them

--- app.js
+++ app.js
@@ -26,8 +26,12 @@ async function getFromServer() {
 }
 
 function renderUsers(users = []) {
-	const html = users.map(toHTML).join('')
-	list.innerHTML = html
+	if (users.length === 0) {
+		list.innerHTML = 'No matched users!'
+	} else {
+		const html = users.map(toHTML).join('')
+		list.innerHTML = html
+	}
 }
 
 function toHTML(oneUser) {
